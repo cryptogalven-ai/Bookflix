8ac5b75204bc84246abc4eb018e015f183fd7cf5
cryptogalven-ai <cryptogalven@gmail.com>
2026-09-12 21:12:59 +0200

    Bump offline cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bookflix-shell-v1';
+const CACHE='bookflix-shell-v2';
 const SHELL=['/','/index.html','/manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
